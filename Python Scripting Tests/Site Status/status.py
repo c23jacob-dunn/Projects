a817521f13922150970905494a6c699d142b67b7
c23jacob-dunn <jdunn1101@gmail.com>
2024-06-04 16:51:11 -0400

    d

---
+++ Python Scripting Tests/Site Status/status.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import os
+
+########################
+# This script will check the status of various websites
+# This could be used to verify your internet connection
+# or to check if your internal servers are functioning.
+#######################
+
+def ping(mysite):
+    #myping = ("ping -q -c 1 %s > /dev/null" % mysite)
+    myping = ("ping -n 1 " + mysite)
+    status = os.system(myping)
+    return(status)
+
+with open("sites.txt") as file:
+    mysites = file.readlines()
+
+for site in mysites:
+    mystatus = ping(site.strip())
+    if mystatus == 0:
+         print(site.strip() + " is fine")
+    if mystatus != 0:
+         print("********************")
+         print("vvvvvvvvvvvvvvvvvvvv")
+         print("%s is down!" % (site.strip()))
+         print("^^^^^^^^^^^^^^^^^^^^")
+         print("********************")
+    print("-----------")
